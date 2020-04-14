f9930b142db0bbe87917ee20d4c5f6f342e5778f
Seba Herrera <seherreraluna@gmail.com>
2020-04-14 00:18:20 -0400

    Dos end-points nuevos para recuperar user_type y user_status
    user-adm/user-type
    user-adm/user-status

--- BuffetAPI/src/main/java/com/CAT/BuffetAPI/Controllers/App_UserController.java
+++ BuffetAPI/src/main/java/com/CAT/BuffetAPI/Controllers/App_UserController.java
@@ -7,12 +7,15 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.CAT.BuffetAPI.Entities.App_user;
+import com.CAT.BuffetAPI.Entities.User_status;
+import com.CAT.BuffetAPI.Entities.User_type;
 import com.CAT.BuffetAPI.Services.App_UserService;
 import com.CAT.BuffetAPI.Services.AuthService;
 
@@ -46,7 +49,7 @@ public class App_UserController {
 	
 	
 	@RequestMapping("/users/{Id}")
-	private Optional<App_user> getSpecificUser(HttpServletResponse res, @RequestParam("data") String id,@RequestHeader("token") String token)
+	private Optional<App_user> getSpecificUser(HttpServletResponse res, @PathVariable("Id") String id, @RequestHeader("token") String token)
 	{
 		List<String> typesAllowed = new ArrayList<String>();
 		typesAllowed.add("ADM");
@@ -62,6 +65,55 @@ public class App_UserController {
 		}
 	}
 	
+	@RequestMapping("/user-type")
+	private List<User_type> getAllTypes(HttpServletResponse res){
+
+		try {
+			// Get the all the Users
+			List<User_type> typeList = app.getAllTypes();
+
+			if(typeList == null){
+				// 404 Not Found
+				res.setStatus(404);
+				return null;
+			}
+
+			// 200 OK
+			res.setStatus(200);
+			return typeList;
+
+		} catch (Exception e) {
+			// If There was an error connecting to the server
+			// 500 Internal Server Error
+			res.setStatus(500);
+			return null;
+		}
+	}
+
+	@RequestMapping("/user-status")
+	private List<User_status> getAllStatys(HttpServletResponse res){
+
+		try {
+			// Get the all the Users
+			List<User_status> typeList = app.getAllStatus();
+
+			if(typeList == null){
+				// 404 Not Found
+				res.setStatus(404);
+				return null;
+			}
+
+			// 200 OK
+			res.setStatus(200);
+			return typeList;
+
+		} catch (Exception e) {
+			// If There was an error connecting to the server
+			// 500 Internal Server Error
+			res.setStatus(500);
+			return null;
+		}
+	}
 
 	/*
 	@RequestMapping("/app_users")
